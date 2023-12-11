e651391c23aa085b9f658a6a3516af2ef94bb3b8
ttommie <devtommie@gmail.com>
2023-12-10 23:37:40 -0500

    feature(events): Adds Event Handler w/ Ready Event

---
+++ src/events/ready.js
@@ -0,0 +1,8 @@
+const { PREFIX } = require('../../config');
+const { ActivityType } = require('discord.js');
+
+module.exports = async (client) => {
+  client.logger.info(`[!] ${client.user.username} is now started...`);
+  client.logger.info(`[!] greg-project has ${client.commands.size} (${PREFIX}) commands and ${client.slash.size} (/) commands`);
+  client.user.setActivity(`${PREFIX}help | github.com/ttommie`, { type: ActivityType.Playing });
+};
