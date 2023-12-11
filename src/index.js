983f92c59d8314971b2f313e3bb71027d49e0347
ttommie <devtommie@gmail.com>
2023-12-11 04:38:05 -0500

    feature(index): Adds Index Entry Point

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+
+// Client
+const client = new Client({
+  allowedMentions: { parse: ['users', 'roles'] },
+  fetchAllMembers: false,
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.MessageContent ],
+});
+
+// Config
+client.config = require('./../config.js');
+
+// Collections
+client.commands = new Collection();
+client.slash = new Collection();
+client.aliases = new Collection();
+
+// Utilities
+client.logger = require('./utils/logger.js');
+client.color = require('./utils/colors.js');
+
+// Handlers
+['events'].forEach(file => {
+  require(`./handlers/${file}`)(client);
+});
+
+// Login
+client.login(client.config.TOKEN_KEY);
