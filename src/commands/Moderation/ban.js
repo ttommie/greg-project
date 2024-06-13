a7b6a56c435e00813ba3b7306dcb6efee705c73e
ttommie <devtommie@gmail.com>
2024-06-13 03:13:56 -0400

    fix(ban): Allow ban by ID to guild members

--- src/commands/Moderation/ban.js
+++ src/commands/Moderation/ban.js
@@ -12,38 +12,49 @@ module.exports = {
   permissions: [PermissionsBitField.Flags.BanMembers],
 
   async execute(client, message, args) {
-    const target = message.mentions.members.first();
-    const reason = args.slice(1).join(' ') || 'no reason specified.';
-    const targetHighestRole = target.roles.highest.position;
+    try {
+      const target =
+        await message.mentions.members.first() || // check tag: @tag
+        await message.guild.members.fetch(args[0]);// check ID: '1147217835265896522'
 
-    if (target.id === message.member.id) {
-      CommandError(message, ERR_MESSAGES.banSelf);
-      return;
+      // FIXME: NOT WORKING YET
+      // await client.users.cache.get((r) => r.user.username.toLowerCase() === args[0].toLocaleLowerCase());// check username
+      // message.guild.members.cache.find((ro) => ro.displayName.toLowerCase() === args[0].toLocaleLowerCase()); // check display name
+
+      const reason = args.slice(1).join(' ') || 'no reason specified.';
+      const targetHighestRole = target.roles.highest.position;
+
+      if (target.id === message.member.id) {
+        CommandError(message, ERR_MESSAGES.banSelf);
+        return;
+      }
+
+      if (message.member.roles.highest.position < targetHighestRole) {
+        CommandError(message, ERR_MESSAGES.permDiff);
+        return;
+      }
+
+      if (reason.length > 512) {
+        CommandError(message, ERR_MESSAGES.reasonSize);
+        return;
+      }
+
+      if (!target.bannable) {
+        CommandError(message, ERR_MESSAGES.notBannable);
+        return;
+      }
+
+      const banEmbed = new EmbedBuilder();
+      banEmbed.setColor('#36393F');
+      banEmbed.setAuthor({ name: '🚨 | Greg Project - $ban', url: 'https://github.com/ttommie/greg-project/' });
+      banEmbed.setDescription(`${target} has been successfully banned.`);
+      banEmbed.addFields({ name: 'Reason:', value: `${reason}`, inline: false });
+
+      // BAN USER HERE: target.ban({ deleteMessage, reason });
+
+      message.channel.send({ embeds: [banEmbed] });
+    } catch (e) {
+      client.logger.error(`${e}`);
     }
-
-    if (message.member.roles.highest.position < targetHighestRole) {
-      CommandError(message, ERR_MESSAGES.permDiff);
-      return;
-    }
-
-    if (reason.length > 512) {
-      CommandError(message, ERR_MESSAGES.reasonSize);
-      return;
-    }
-
-    if (!target.bannable) {
-      CommandError(message, ERR_MESSAGES.notBannable);
-      return;
-    }
-
-    const banEmbed = new EmbedBuilder();
-    banEmbed.setColor('#36393F');
-    banEmbed.setAuthor({ name: '🚨 | Greg Project - $ban', url: 'https://github.com/ttommie/greg-project/' });
-    banEmbed.setDescription(`${target} has been successfully banned.`);
-    banEmbed.addFields({ name: 'Reason:', value: `${reason}`, inline: false });
-
-    // BAN USER HERE: target.ban({ deleteMessage, reason });
-
-    message.channel.send({ embeds: [banEmbed] });
   },
 };
